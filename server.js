18155ac406aa440a7381c61e523e40c1de191ff5
fernando <sombarepam@gmail.com>
2025-11-03 14:41:20 -0300

    feat: configure server with Stream and Genero routes

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
+import streamRoutes from './src/routes/streamRoutes.js';
+import generoRoutes from './src/routes/generoRoutes.js';
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-dotenv.config();
 const serverPort = process.env.PORT || 3001;
 
 app.get("/", (req, res) => {
-    res.send("Servidor funcionando...");
+    res.send("🚀 Servidor funcionando...");
 });
+
+app.use('/streams', streamRoutes);
+app.use('/generos', generoRoutes);
 
 app.listen(serverPort, () => {
     console.log(`🚀 Servidor rodando em http://localhost:${serverPort} 🚀`);
